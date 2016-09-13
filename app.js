743bfcb42c50e9701f53e52333bd8cef385d6218
Alex <alexgregorieart@gmail.com>
2016-09-13 13:06:08 -0400

    Dots and Info
    Added More dots as well as dummy info. Also separated the name from the
    title of the location into separate key:value pairs.
    
    Working on modifying the info-panel to be more useful. Model photos and
    store links

--- app.js
+++ app.js
@@ -9,38 +9,78 @@ var windowWidth = window.innerWidth;
 //The dots array holds all of the objects for the location data on the map.
 var dots = [
     {
+        name: 'error',
+        title:'Unexplored Areas',
+        blurb: 'Here there be untold dangers that you may never live to see.'
+        },
+    {
         name: 'dragyri-1',
+        title: 'Air Caste Home',
         blurb: 'Beef tenderloin pancetta hamburger porchetta pork, pastrami pork loin chuck pork belly flank prosciutto ball tip. Shank alcatra cow filet mignon doner beef ball tip jowl kielbasa hamburger pork. Brisket beef tri-tip biltong porchetta chicken capicola short ribs bresaola filet mignon drumstick. Short loin capicola shoulder, doner bacon ribeye biltong shankle rump landjaeger. Short ribs tenderloin t-bone pork, pig short loin spare ribs turducken andouille. Turkey jowl strip steak, chuck tongue cupim ribeye pork loin sirloin andouille ham pork spare ribs bacon.'},
     {
         name: 'dragyri-2',
+        title: 'Fire Caste Dungeon',
         blurb: 'Chicken tail pork chop jerky beef alcatra. Bresaola landjaeger shank, short loin jowl meatball beef porchetta doner ham hock bacon. Shank porchetta bacon ground round leberkas short ribs chuck ribeye. Jerky pastrami rump, meatloaf jowl boudin short loin pork belly picanha beef ribs bacon. Chicken ground round venison kevin. Meatloaf biltong shoulder, turducken turkey tail drumstick ground round prosciutto short ribs pork loin porchetta filet mignon'
     },
     {
         name: 'dragyri-3',
+        title: 'That other Place',
         blurb: 'Filet mignon bacon strip steak capicola ball tip chuck pork. Drumstick sirloin beef fatback, picanha porchetta ground round shank shoulder. Beef short loin spare ribs turkey pancetta picanha. Alcatra flank pig ribeye pastrami ham. Jowl t-bone tongue short ribs pork, sausage rump tri-tip shankle jerky turducken salami bacon. Pork loin corned beef cow meatball kevin fatback turducken kielbasa t-bone chicken short loin drumstick brisket jerky tri-tip. Venison boudin beef ribs shoulder, sirloin swine salami chicken meatloaf beef ball tip filet mignon t-bone.'
     },
     {
         name: 'dragyri-4',
+        title: 'The Ancient Ruins of Ben Al Durchenab',
         blurb: 'Drumstick short ribs salami alcatra bresaola, picanha cupim. Beef ribs brisket sirloin prosciutto biltong, ham hock jowl corned beef pork belly meatball shankle short ribs t-bone. Pancetta tail ribeye, pork loin ground round shank capicola alcatra prosciutto chicken tri-tip. Pork belly jowl capicola venison pork loin cow filet mignon ribeye pork chop.'
+    },
+    {
+        name: 'dragyri-5',
+        title: 'Brood Hive',
+        blurb: 'Drumstick short ribs salami alcatra bresaola, picanha cupim. Beef ribs brisket sirloin prosciutto biltong, ham hock jowl corned beef pork belly meatball shankle short ribs t-bone. Pancetta tail ribeye, pork loin ground round shank capicola alcatra prosciutto chicken tri-tip. Pork belly jowl capicola venison pork loin cow filet mignon ribeye pork chop.'
+    },
+    {
+        name: 'dragyri-6',
+        title: 'Recent Deaths',
+        blurb: 'Drumstick short ribs salami alcatra bresaola, picanha cupim. Beef ribs brisket sirloin prosciutto biltong, ham hock jowl corned beef pork belly meatball shankle short ribs t-bone. Pancetta tail ribeye, pork loin ground round shank capicola alcatra prosciutto chicken tri-tip. Pork belly jowl capicola venison pork loin cow filet mignon ribeye pork chop.'
+    },
+    {
+        name: 'dragyri-7',
+        title: 'New Ashkelon',
+        blurb: 'Drumstick short ribs salami alcatra bresaola, picanha cupim. Beef ribs brisket sirloin prosciutto biltong, ham hock jowl corned beef pork belly meatball shankle short ribs t-bone. Pancetta tail ribeye, pork loin ground round shank capicola alcatra prosciutto chicken tri-tip. Pork belly jowl capicola venison pork loin cow filet mignon ribeye pork chop.'
+    },
+    {
+        name: 'dragyri-8',
+        title: 'Toxic Waste Dump',
+        blurb: 'vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis'
+    },
+    {
+        name: 'newAsh',
+        title: 'New Ashkelon',
+        blurb: 'iatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure'
+    },{
+        name: 'dragyri-10',
+        title: 'CORE Outpost',
+        blurb: ' Robot ipsum datus scan amet, constructor ad ut splicing elit, sed do errus mod tempor in conduit ut laboratory et deplore electromagna aliqua. Ut enim ad minimum veniam, quis no indestruct exoform ullamco laboris nisi ut alius equip ex ea commando evaluant. Duis ex machina aute ire dolorus in scan detectus an voluptate volt esse cesium dolore eu futile nulla parameter. Execute primus sint occaecat cupidatat non proident, sunt in culpa qui technia deserunt mondus anim id est proceus.'
     },
 ];
 
 //get the name attribute of the location element
 function getNameInfo(n){
-    for(var i = 0; i<dots.length; i++){
+    for(var i = 1; i<dots.length; i++){
         var x =  dots[i].name;
         if (x == n){
-            console.log(dots[i]);
+            console.log('success');
             renderInformation(dots[i]);
+        }else if(i>=dots.length){
+            console.log('no match');
+            renderInformation(dots[0]);
         }
-        console.log(x);
     }
 }
 //render the info to the DOM
 function renderInformation(obj){
     //take the object
     //split it into the proper data
-    var n = obj.name;
+    var n = obj.title;
     var b = obj.blurb;
     console.log(n,b);
     //render each piece to the DOM
@@ -49,8 +89,8 @@ function renderInformation(obj){
         document.getElementById('location-blurb').innerHTML = b;
     }else {
         console.log('unknown')
-        n = "Danger!"
-        b = "Unknown Location. Send Recon unit soon"
+        document.getElementById('location-title').innerHTML = "Danger!"
+        document.getElementById('location-blurb').innerHTML = "Unknown Location. Send Recon unit soon"
     }
 
     //if the Object is unknown or the key is missing replace with some dummy text
